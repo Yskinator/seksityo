fd16f91d976f99534c7de544efba10a38bb7dfc1
ilarinie <ilari.nieminen@outlook.com>
2017-01-23 11:15:49 +0200

    New unit test for MeetController to test travis compability.

--- src/test/java/com/example/controller/MeetControllerTests.java
+++ src/test/java/com/example/controller/MeetControllerTests.java
@@ -2,26 +2,33 @@ package com.example.controller;
 
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
 import java.util.Collections;
 
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MeetControllerTests {
 
-    @Test
-    public void meetControllerReturnsOk() throws Exception {
-        /*String res = "";
-        TestRestTemplate rest = new TestRestTemplate();
-        ResponseEntity<String> response = rest.postForEntity("http://localhost:8080/ok", res, String.class, Collections.emptyMap());
+    @Autowired
+    private TestRestTemplate restTemplate;
 
-        assert(response.getStatusCode() == HttpStatus.OK );
-        */
-        assert(true);
+    @Test
+    public void okTest(){
+        String res = "";
+        ResponseEntity<String> response = this.restTemplate.postForEntity("/ok", res, String.class, Collections.emptyMap());
+        assert(response.getStatusCode() == HttpStatus.OK);
     }
+
+
 
 
 }
