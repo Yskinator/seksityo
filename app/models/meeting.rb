145b4a6092160be22e1a71589c3ba3f4dcb1c88f
Merloni <tuomo.salmenkivi@gmail.com>
2017-01-27 13:02:06 +0200

    Created a mobilefriendlier view for creating a new meeting.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,5 +2,6 @@ class Meeting < ActiveRecord::Base
   validates :duration, numericality: { greater_than: 0,
                                        only_integer: true,
                                        less_than: 1440 }, allow_blank: true
+
   validates :phone_number, phone: true
 end
