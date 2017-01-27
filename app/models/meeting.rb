3cc28db72a3e30df958227fc8341937d08968e8d
Hilla Fred <hilla.fred@helsinki.fi>
2017-01-27 10:33:35 +0200

    Time input validation.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,5 @@
 class Meeting < ActiveRecord::Base
+  validates :duration, numericality: { greater_than: 0,
+                                       only_integer: true,
+                                       less_than: 1440 }
 end
