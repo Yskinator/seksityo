2f5ba00b14b211993445f8f61f71334c4ba921fe
Hilla Fred <hilla.fred@helsinki.fi>
2017-02-01 14:21:01 +0200

    Added test for status page showing duration.

--- spec/features/start_new_meeting_spec.rb
+++ spec/features/start_new_meeting_spec.rb
@@ -9,4 +9,12 @@ feature 'Start new meeting' do
 
     expect(page).to have_content('Sending message at:')
   end
+  scenario 'user sees duration' do
+    visit '/'
+    fill_in 'meeting_phone_number', with: '0401234123'
+    fill_in 'duration', with: '30'
+    click_button 'Start timer'
+
+    expect(page).to have_content('30 minutes')
+  end
 end
