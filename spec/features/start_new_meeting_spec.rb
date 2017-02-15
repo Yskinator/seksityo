fcf4ca541da3181b186f89b57bfcdd46f5f0dd73
Yskinator <yski2moreletters@gmail.com>
2017-02-15 11:37:08 +0200

    Added tests for validated emails. Fixed broken tests.

--- spec/features/start_new_meeting_spec.rb
+++ spec/features/start_new_meeting_spec.rb
@@ -5,16 +5,40 @@ feature 'When starting a new meeting' do
   scenario 'user can create a meeting', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Pekka'
-    fill_in 'meeting_phone_number', with: '0401234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     fill_in 'duration-input', with: '111'
     click_button 'Start timer'
 
     expect(page).to have_content('Sending message at:')
   end
+  scenario 'user cannot create a meeting with an incorrect email', js:true do
+    visit '/meetings/new'
+    fill_in 'meeting_nickname', with: 'Pekka'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail'
+    fill_in 'duration-input', with: '111'
+    click_button 'Start timer'
+
+    expect(page).to have_content('Phone number is invalid')
+
+    fill_in 'meeting_nickname', with: 'Pekka'
+    fill_in 'meeting_phone_number', with: '@gmail.com'
+    fill_in 'duration-input', with: '111'
+    click_button 'Start timer'
+
+    expect(page).to have_content('Phone number is invalid')
+
+    fill_in 'meeting_nickname', with: 'Pekka'
+    fill_in 'meeting_phone_number', with: 'seksityo'
+    fill_in 'duration-input', with: '111'
+    click_button 'Start timer'
+
+    expect(page).to have_content('Phone number is invalid')
+
+  end
   scenario 'user sees manually input duration on status page', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Matti'
-    fill_in 'meeting_phone_number', with: '0501231231'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     fill_in 'duration-input', with: '55'
     click_button 'Start timer'
 
@@ -23,7 +47,7 @@ feature 'When starting a new meeting' do
   scenario 'user sees selected 2h duration on status page', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Sami'
-    fill_in 'meeting_phone_number', with: '0441234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     find('#select2h').click
     click_button 'Start timer'
     expect(page).to have_content('120 minutes')
@@ -31,7 +55,7 @@ feature 'When starting a new meeting' do
   scenario 'user sees selected 1h duration on status page', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Jyrki'
-    fill_in 'meeting_phone_number', with: '0461234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     find('#select1h').click
     click_button 'Start timer'
     expect(page).to have_content('60 minutes')
@@ -39,7 +63,7 @@ feature 'When starting a new meeting' do
   scenario 'user sees selected 30min duration on status page', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Hilla'
-    fill_in 'meeting_phone_number', with: '0411234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     fill_in 'duration-input', with: '115'
     click_button 'Start timer'
     expect(page).to have_content('115 minutes')
@@ -47,7 +71,7 @@ feature 'When starting a new meeting' do
   scenario 'user selects duration and then manually enters one', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Tatti'
-    fill_in 'meeting_phone_number', with: '0421234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     find('#select30m').click
     fill_in 'duration-input', with: '75'
     click_button 'Start timer'
@@ -56,7 +80,7 @@ feature 'When starting a new meeting' do
   scenario 'user manually enters duration and then selects one', js: true do
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Laura'
-    fill_in 'meeting_phone_number', with: '0471234123'
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     fill_in 'duration-input', with: '75'
     find('#select2h').click
     click_button 'Start timer'
