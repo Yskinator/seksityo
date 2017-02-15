0e7006287e6643503c9ac1a777ccddd4207b65f7
Hilla Fred <hilla.fred@helsinki.fi>
2017-02-15 12:15:37 +0200

    Added feature tests for emails.

--- spec/features/application_mailer_spec.rb
+++ spec/features/application_mailer_spec.rb
@@ -1,19 +1,30 @@
 require 'rails_helper'
 
 describe 'Mailer' do
-
-  it 'should send message when the timer goes off', js: true do
+  before :each do
     clear_emails
     Delayed::Worker.delay_jobs = false
     visit '/meetings/new'
     fill_in 'meeting_nickname', with: 'Pekka'
-    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     fill_in 'duration-input', with: '1'
+  end
+
+  after :each do
+    Delayed::Worker.delay_jobs = true
+  end
+
+  it 'should send message when the timer goes off', js: true do
+    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
     click_button 'Start timer'
     sleep(0.1)
-    # Will find an email sent to seksityo@gmail.com and set 'current_email'
     open_email('seksityo@gmail.com')
     expect(current_email).to have_content "Assumed duration for friend's activity has passed."
-    Delayed::Worker.delay_jobs = true
+  end
+  it 'should send the email to the address specified by user', js: true do
+    fill_in 'meeting_phone_number', with: 'tyoseksi@gmail.com'
+    click_button 'Start timer'
+    sleep(0.1)
+    open_email('seksityo@gmail.com')
+    expect(current_email).to be(nil)
   end
 end
