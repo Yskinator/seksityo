4e61f89a038ecc392c56609495d5ef443ff1e3a7
Yskinator <yski2moreletters@gmail.com>
2017-02-17 09:34:23 +0200

    Renamed test emails for clarity.

--- spec/features/application_mailer_spec.rb
+++ spec/features/application_mailer_spec.rb
@@ -14,26 +14,26 @@ describe 'Mailer' do
   end
 
   it 'should send message when the timer goes off', js: true do
-    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
+    fill_in 'meeting_phone_number', with: 'testi@testi.test'
     click_button 'Start timer'
     sleep(0.1)
-    open_email('seksityo@gmail.com')
+    open_email('testi@testi.test')
     expect(current_email).to have_content "The assumed duration for Pekka's activity has passed."
   end
   it 'should send the email to the address specified by user', js: true do
-    fill_in 'meeting_phone_number', with: 'tyoseksi@gmail.com'
+    fill_in 'meeting_phone_number', with: 'testi2@testi.test'
     click_button 'Start timer'
     sleep(0.1)
-    open_email('seksityo@gmail.com')
+    open_email('testi@testi.test')
     expect(current_email).to be(nil)
   end
   it 'should send message when the alert button is pressed', js: true do
-    fill_in 'meeting_phone_number', with: 'seksityo@gmail.com'
+    fill_in 'meeting_phone_number', with: 'testi@testi.test'
     click_button 'Start timer'
     expect(page).to have_button("Send Alert!")
     click_button 'Send Alert!'
     sleep(0.1)
-    open_email('seksityo@gmail.com')
+    open_email('testi@testi.test')
     expect(current_email).to have_content "is in trouble and needs help immediately."
   end
 end
