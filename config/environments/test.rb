6c57306e66d364b3544095d1c03d082191a625f0
ilarinie <ilari.nieminen@outlook.com>
2017-02-24 15:00:14 +0200

    Suppressed the very verbose test output, now only outputting test failures.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost',
                                                port: 3001 }
+
+  config.log_level = :fatal
+
 end
