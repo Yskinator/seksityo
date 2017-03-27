de3d157845a1fb0f14a910acdf495961ec4f12d2
Yskinator <yski2moreletters@gmail.com>
2017-03-27 16:09:18 +0300

    Added tests for incrementing stats.

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Stat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has its values properly initialized to zeroes" do
+    stat = Stat.new
+    expect(stat.notifications_sent).to eq(0)
+    expect(stat.alerts_sent).to eq(0)
+    expect(stat.confirmed).to eq(0)
+    expect(stat.created).to eq(0)
+  end
+  it "increments country's first creation stat correctly" do
+    Stat.increment_created("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.created).to eq(1)
+  end
+  it "increments country's subsequent creation stats correctly" do
+    Stat.increment_created("123")
+    Stat.increment_created("123")
+    Stat.increment_created("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.created).to eq(3)
+  end
+  it "increments a second country's creation stats correctly" do
+    Stat.increment_created("123")
+    Stat.increment_created("124")
+    stat = Stat.find_by_country_code("124")
+    expect(stat.created).to eq(1)
+  end
+  it "increments country's first confirmation stat correctly" do
+    Stat.increment_confirmed("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.confirmed).to eq(1)
+  end
+  it "increments country's subsequent confirmation stats correctly" do
+    Stat.increment_confirmed("123")
+    Stat.increment_confirmed("123")
+    Stat.increment_confirmed("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.confirmed).to eq(3)
+  end
+  it "increments a second country's confirmation stats correctly" do
+    Stat.increment_confirmed("123")
+    Stat.increment_confirmed("124")
+    stat = Stat.find_by_country_code("124")
+    expect(stat.confirmed).to eq(1)
+  end
+  it "increments country's first sent notification stat correctly" do
+    Stat.increment_notifications_sent("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.notifications_sent).to eq(1)
+  end
+  it "increments country's subsequent sent notifications stats correctly" do
+    Stat.increment_notifications_sent("123")
+    Stat.increment_notifications_sent("123")
+    Stat.increment_notifications_sent("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.notifications_sent).to eq(3)
+  end
+  it "increments a second country's sent notifications stats correctly" do
+    Stat.increment_notifications_sent("123")
+    Stat.increment_notifications_sent("124")
+    stat = Stat.find_by_country_code("124")
+    expect(stat.notifications_sent).to eq(1)
+  end
+  it "increments country's first sent alerts stat correctly" do
+    Stat.increment_alerts_sent("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.alerts_sent).to eq(1)
+  end
+  it "increments country's subsequent sent alerts stats correctly" do
+    Stat.increment_alerts_sent("123")
+    Stat.increment_alerts_sent("123")
+    Stat.increment_alerts_sent("123")
+    stat = Stat.find_by_country_code("123")
+    expect(stat.alerts_sent).to eq(3)
+  end
+  it "increments a second country's sent alerts stats correctly" do
+    Stat.increment_alerts_sent("123")
+    Stat.increment_alerts_sent("124")
+    stat = Stat.find_by_country_code("124")
+    expect(stat.alerts_sent).to eq(1)
+  end
 end
