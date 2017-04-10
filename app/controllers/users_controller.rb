ba42a903ddf0abec76d18872934d815d9858f7f9
Yskinator <yski2moreletters@gmail.com>
2017-04-10 14:55:49 +0300

    Added validations and bugfixes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   # POST /users/
   def create_or_find
-    @user = User.find_by_phone_number(params[:phone_number])
-
+    @user = User.find_by_phone_number(user_params[:phone_number])
     if !@user
       cookies.delete 'code'
-      @user = User.new
+      @user = User.new(user_params)
+      @user.parse_phone_number
       @user.create_code
       if @user.save
         cookies['code'] = @user.code
@@ -21,6 +21,12 @@ class UsersController < ApplicationController
   # GET /users/
   def phone_form
     @user = User.new
+  end
+
+  private
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:phone_number)
   end
 
 end
