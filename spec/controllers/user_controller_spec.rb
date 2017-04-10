61358120a504db5df1f6bf1d1e0eb20de59609f8
Yskinator <yski2moreletters@gmail.com>
2017-04-10 14:56:07 +0300

    Added tests.

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,41 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET phone_input" do
+  describe "GET users" do
     it "renders the phone input page" do
       get :phone_form
       expect(response).to render_template("phone_form")
     end
+  end
+  describe "POST users" do
+    user_params = {:phone_number => "0401231234"}
+    it "creates a new user if it does not exist" do
+      expect { post :create_or_find, :user => user_params }.to change(User, :count).by(1)
+    end
     it "generates a code for the user" do
-      @user = User.first
-      expect(@user).to equal(nil)
-      get :create_or_find
-      @user = User.first
+      post :create_or_find, :user => user_params
+      @user = User.find_by_phone_number("0401231234")
       expect(@user.code).not_to equal(nil)
     end
     it "adds the code to cookies" do
-      get :create_or_find
+      post :create_or_find, :user => user_params
       @user = User.first
       expect(@response.cookies['code']).to eq(@user.code)
     end
-    it "does not generate a new code if the user already has one" do
-      @user = User.new
-      @user.create_code
-      @request.cookies['code'] = @user.code
-      @user.save
-      get :create_or_find
-      expect(@request.cookies['code']).to eq(@user.code)
-    end
-    it "adds new user to database" do
-      expect(User.all.length).to eq(0)
-      get :create_or_find
-      expect(User.all.length).to eq(1)
-    end
-    it "generates a new code if the user found in cookies not found in database" do
-      @request.cookies['code'] = 'wrongcookie'
-      get :create_or_find
-      @user = User.first
-      expect(@response.cookies['code']).to eq(@user.code)
+    it "does not create a new user if a user corresponding to the number already exists" do
+      post :create_or_find, :user=>user_params
+      expect { post :create_or_find, :user => user_params }.to change(User, :count).by(0)
     end
   end
 end
