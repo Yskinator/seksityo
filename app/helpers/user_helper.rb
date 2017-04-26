4c9eb79d1edf4c885769595bf4ff08f79989854b
Yskinator <yski2moreletters@gmail.com>
2017-04-26 10:51:13 +0300

    Revert "Made the credits_enabled check its own before_action-like method."
    This reverts commit c7766651c38d785630dfd6a89a11102fdfd1f15d.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,28 +1,27 @@
 module UserHelper
   @credits_enabled = false
 
-  # This check happens before every method
-  # Returns true so that validate_user always returns true when credits are disabled
-  def self.included(base)
-    unless @credits_enabled
-      return true
+  def decrease_credits
+    if @credits_enabled
+      @user = User.find_by_code(cookies[:ucd])
+      @user.credits -= 1
+      @user.save
     end
   end
 
-  def decrease_credits
-    @user = User.find_by_code(cookies[:ucd])
-    @user.credits -= 1
-    @user.save
-  end
-
   def increment_credits
-    @user = User.find_by_code(cookies[:ucd])
-    @user.credits += 1
-    @user.save
+    if @credits_enabled
+      @user = User.find_by_code(cookies[:ucd])
+      @user.credits += 1
+      @user.save
+    end
   end
 
   # Validates that the user exists and has credits
   def validate_user
+    unless @credits_enabled
+      return true
+    end
     unless user_exists
       redirect_to users_path
       return
